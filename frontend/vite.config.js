ba80380301000d8086382e2e66ff56ece1493311
Shakshi Sharma <shaksshisharma@gmail.com>
2026-03-24 22:03:09 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        // CHANGE THIS IP to the exact IP address of your other laptop!
+        // Example: 'http://192.168.1.25:5001'
+        target: 'http://localhost:5001',
+        changeOrigin: true
+      }
+    }
+  }
 })
